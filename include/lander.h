6fb8b8d8393e36428c23cd69911d82d3fdcf7b19
Angelina <angelina.boynton@outlook.com>
2025-07-27 19:48:22 -0700

    Phase 1: Lander Class created

---
+++ include/lander.h
@@ -0,0 +1,50 @@
+#pragma once
+
+
+/**
+Lander Class: 
+defines and tracks altitude, velocity, and fuel
+*/
+
+class Lander {
+    public:
+    /** Constructor for a new Lander Object
+    * \param altitude: starting height in meters above surface.
+    * \param fuel: intial fuel supply available for descent.
+    */
+    Lander(double altitude, double fuel);
+
+    /** applyThrust Member Function: applies thrust to the lander.
+    * \param amount: Thrust power as a fraction (0.0 = no thrust, 1.0 = max thrust).
+    * If fuel is insufficient, thrust will be proportionally limited.
+    */    
+    void applyThrust(double amount);
+
+    /** update Member Function: updates the velocity of the lander.
+    * @param dt Time step in seconds.
+    * Applies gravity and updates velocity and altitude accordingly.
+    * Detects landing or crashing conditions    
+    */    
+    void update(double dt);
+
+    double getAltitude() const;        // Get current altitude in meters.
+    double getVelocity() const;        // Get current velocity in m/s (positive means falling).
+    double getFuel() const;            // Get remaining fuel units
+    bool hasLanded() const;            // Check if the lander has landed (altitude == 0).
+    bool hasCrashed() const;           // Check if the lander has crashed (landed too fast)
+
+    private:
+    // Current altitude, velocity, and remaining fuel 
+    double m_altitude;
+    double m_velocity;
+    double m_fuel;
+
+
+    const double gravity = 3.71;        // Mars gravity (m/s²)
+    const double maxThrust = 10.0;      // Max acceleration from thrust in m/s²
+    const double fuelBurnRate = 0.5;    // Fuel units per second at full thrust
+
+    bool landed = false;                // True if lander has touched down.
+    bool crashed = false;               // True if lander landed with unsafe velocity.
+
+}
